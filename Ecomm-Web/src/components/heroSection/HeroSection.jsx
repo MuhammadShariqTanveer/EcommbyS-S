5c955b17afe603ff48369192579423f3bc089c01
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-10-21 21:52:45 +0500

    Project

---
+++ Ecomm-Web/src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+    return (
+        <div className="bg-contain">
+           <img className="w-full h-auto" src="../img/hero_change.jpg" alt="" />
+        </div>
+    );
+}
+
+export default HeroSection;
